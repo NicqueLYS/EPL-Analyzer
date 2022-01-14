ae59a6d9368bca2a2132cb05c87b5805ce20ab84
Kah Chen <kah_chen_chan@dell.com>
2022-01-14 15:46:46 +0800

    clean comment

--- core/core/settings.py
+++ core/core/settings.py
@@ -21,10 +21,6 @@ import os
 from decouple import config
 from unipath import Path
 
-"""
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-"""
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).parent
@@ -44,21 +40,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-"""
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = Path(__file__).parent
-CORE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY', default='S#perS3crEt_1122')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
-
-# load production server from .env
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', config('SERVER', default='127.0.0.1')]
-"""
 
 
 # Application definition
@@ -170,15 +151,3 @@ STATICFILES_DIRS = (
 
 #############################################################
 #############################################################
-
-"""
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-"""
